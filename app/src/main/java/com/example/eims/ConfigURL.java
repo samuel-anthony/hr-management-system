66162e81db89773728bea71def1ad498fe00a2b7
cigra <cindy.zebua@gmail.com>
2019-10-18 23:28:53 +0700

    Update IP

--- app/src/main/java/com/example/eims/ConfigURL.java
+++ app/src/main/java/com/example/eims/ConfigURL.java
@@ -3,6 +3,6 @@ package com.example.eims;
 public class ConfigURL {
     //ini buat constant yang di pake di db nantinya
     //ip wifi arenda =  192.168.0.107
-    private final static String staticIP = "192.168.0.107";
+    private final static String staticIP = "192.168.1.140";
     public final static String Login = "http://" + staticIP + "/eims/login.php/";
 }

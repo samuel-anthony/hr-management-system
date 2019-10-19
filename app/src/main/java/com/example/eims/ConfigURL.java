9f4dcffcce23f9b802eceb54b93dad56c56a1d6e
cigra <cindy.zebua@gmail.com>
2019-10-19 08:26:24 +0700

    Cigra Local Apdet

--- app/src/main/java/com/example/eims/ConfigURL.java
+++ app/src/main/java/com/example/eims/ConfigURL.java
@@ -2,13 +2,10 @@ package com.example.eims;
 
 public class ConfigURL {
     //ini buat constant yang di pake di db nantinya
-    //ip wifi arenda =  192.168.0.107
-<<<<<<< HEAD
+    //ip wifi arenda =  192.168.0.107 | IP Kaisar 2 = 192.168.1.140
+    //ip hape gue = 192.168.43.91  | IP Hp Cindy = 192.168.43.134
+//    private final static String staticIP = "192.168.43.91";
     private final static String staticIP = "192.168.1.140";
-=======
-    //ip hape gue = 192.168.43.91
-    private final static String staticIP = "192.168.43.91";
->>>>>>> master
     public final static String Login = "http://" + staticIP + "/eims/login.php/";
     public final static String CheckAttendanceEmployee = "http://" + staticIP + "/eims/checkAttendanceEmployee.php/";
     public final static String CheckProjectEmployee = "http://" + staticIP + "/eims/checkProjectEmployee.php/";

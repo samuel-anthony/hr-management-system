932d58d5002e99d0b7f7e7b578913f3031a126b0
minatorisato <36133909+minatorisato@users.noreply.github.com>
2019-11-14 08:15:54 +0700

    task kosongan

---
+++ app/src/main/java/com/example/eims/TaskSearch.java
@@ -0,0 +1,14 @@
+package com.example.eims;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_search);
+    }
+}
